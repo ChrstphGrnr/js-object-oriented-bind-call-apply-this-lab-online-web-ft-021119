b7db4cb16f0b066160400b65df446f6b64b6fff2
Christoph Gruener <christophgruener@me.com>
2019-05-28 23:12:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ function setThisWithApply(fn, thisValue, args) {
   return result;
 }
 
-function returnNewFunctionOf(functionToBeCopied, )
+function returnNewFunctionOf(functionToBeCopied, thisValue)
